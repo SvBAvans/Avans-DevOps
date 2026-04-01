ecf238380694102afbc7228ec18ce4d1dd4e7051
Pjiesco <Pjiesco@gmail.com>
2026-04-01 15:46:24 +0200

    Tests: Some more state tests

--- UnitTests/BacklogItemStateFlowTests.cs
+++ UnitTests/BacklogItemStateFlowTests.cs
@@ -17,6 +17,77 @@ public class BacklogItemStateFlowTests
     }
     
     [Fact]
+    public void Todo_StartWork_GoesToDoing()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+
+        Assert.Equal("DoingState", item.GetStateName());
+    }
+    
+    [Fact]
+    public void Doing_ToReadyForTesting()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+
+        Assert.Equal("ReadyForTestingState", item.GetStateName());
+    }
+    
+    [Fact]
+    public void ReadyForTesting_ToTesting()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+
+        Assert.Equal("TestingState", item.GetStateName());
+    }
+    
+    [Fact]
+    public void Testing_ToTested()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+
+        Assert.Equal("TestedState", item.GetStateName());
+    }
+    
+    [Fact]
+    public void Tested_ToDone()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+        item.ApproveDone();
+
+        Assert.Equal("DoneState", item.GetStateName());
+    }
+    
+    [Fact]
+    public void AnyState_ReturnToTodo_Works()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.ReturnToTodo();
+
+        Assert.Equal("TodoState", item.GetStateName());
+    }
+    
+    [Fact]
     public void Cannot_Skip_From_Todo_To_Testing()
     {
         var item = TestHelper.CreateItem();
@@ -53,6 +124,22 @@ public class BacklogItemStateFlowTests
         var item = TestHelper.CreateItem();
 
         item.StartWork();
+        item.ReturnToTodo();
+
+        Assert.Equal("TodoState", item.GetStateName());
+    }
+    
+    [Fact]
+    public void Done_CanReturnToTodo()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+        item.ApproveDone();
+
         item.ReturnToTodo();
 
         Assert.Equal("TodoState", item.GetStateName());
