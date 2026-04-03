eb3488af532d59f1eb908650dc2876897e8ab16b
casstaal <casstaal@hotmail.com>
2026-04-03 15:45:15 +0200

    Added test for backlog when not all activities are done and tests for activity changes when backlog item is closed.

---
+++ UnitTests/ActivityClosedBacklogTests.cs
@@ -0,0 +1,74 @@
+﻿using Avans_DevOps.domain;
+using Xunit;
+
+namespace UnitTests;
+
+public class ActivityClosedBacklogTests
+{
+    private static Activity CreateClosedParentActivity()
+    {
+        var activity = ActivityTestHelper.CreateActivity();
+        activity.Parent.IsClosed = true;
+        return activity;
+    }
+
+    [Fact]
+    public void StartWork_WhenParentBacklogItemIsClosed_Throws()
+    {
+        var activity = CreateClosedParentActivity();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => activity.StartWork());
+
+        Assert.Equal("BacklogItem is closed", ex.Message);
+    }
+
+    [Fact]
+    public void MarkReadyForTesting_WhenParentBacklogItemIsClosed_Throws()
+    {
+        var activity = CreateClosedParentActivity();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => activity.MarkReadyForTesting());
+
+        Assert.Equal("BacklogItem is closed", ex.Message);
+    }
+
+    [Fact]
+    public void MarkTesting_WhenParentBacklogItemIsClosed_Throws()
+    {
+        var activity = CreateClosedParentActivity();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => activity.MarkTesting());
+
+        Assert.Equal("BacklogItem is closed", ex.Message);
+    }
+
+    [Fact]
+    public void MarkTested_WhenParentBacklogItemIsClosed_Throws()
+    {
+        var activity = CreateClosedParentActivity();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => activity.MarkTested());
+
+        Assert.Equal("BacklogItem is closed", ex.Message);
+    }
+
+    [Fact]
+    public void ApproveDone_WhenParentBacklogItemIsClosed_Throws()
+    {
+        var activity = CreateClosedParentActivity();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => activity.ApproveDone());
+
+        Assert.Equal("BacklogItem is closed", ex.Message);
+    }
+
+    [Fact]
+    public void ReturnToTodo_WhenParentBacklogItemIsClosed_Throws()
+    {
+        var activity = CreateClosedParentActivity();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => activity.ReturnToTodo());
+
+        Assert.Equal("BacklogItem is closed", ex.Message);
+    }
+}
---
+++ UnitTests/BacklogItemApproveDoneTests.cs
@@ -0,0 +1,89 @@
+﻿using Avans_DevOps.domain;
+using Xunit;
+
+namespace UnitTests;
+
+public class BacklogItemApproveDoneTests
+{
+    [Fact]
+    public void ApproveDone_WhenNotAllActivitiesAreDone_Throws()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.AddActivity("Activity 1", item.Member);
+        item.AddActivity("Activity 2", item.Member);
+
+        var first = item.Activities[0];
+        first.StartWork();
+        first.MarkReadyForTesting();
+        first.MarkTesting();
+        first.MarkTested();
+        first.ApproveDone();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => item.ApproveDone());
+
+        Assert.Equal("Not all activities are marked as 'Done'", ex.Message);
+    }
+
+    [Fact]
+    public void ApproveDone_WhenAllActivitiesAreDone_GoesToDone()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.AddActivity("Activity 1", item.Member);
+        item.AddActivity("Activity 2", item.Member);
+
+        foreach (var activity in item.Activities)
+        {
+            activity.StartWork();
+            activity.MarkReadyForTesting();
+            activity.MarkTesting();
+            activity.MarkTested();
+            activity.ApproveDone();
+        }
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+        item.ApproveDone();
+
+        Assert.Equal("DoneState", item.GetStateName());
+    }
+
+    [Fact]
+    public void ApproveDone_WhenThereAreNoActivities_UsesNormalStateFlow()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+        item.ApproveDone();
+
+        Assert.Equal("DoneState", item.GetStateName());
+    }
+
+    [Fact]
+    public void ApproveDone_WhenActivityIsStillTodo_Throws()
+    {
+        var item = TestHelper.CreateItem();
+
+        item.AddActivity("Activity 1", item.Member);
+
+        item.StartWork();
+        item.MarkReadyForTesting();
+        item.MarkTesting();
+        item.MarkTested();
+
+        var ex = Assert.Throws<InvalidOperationException>(() => item.ApproveDone());
+
+        Assert.Equal("Not all activities are marked as 'Done'", ex.Message);
+    }
+}
